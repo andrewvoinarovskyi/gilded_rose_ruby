d775ba3e861f4e61be978b3d39df4614c1c974c3
andrii <avoinarovskiy@gmail.com>
2025-03-17 16:38:41 +0200

    improve shared exampleі for specs

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -12,7 +12,7 @@ describe GildedRose do
     GildedRose.new(@items).update_quality
   end
 
-  shared_examples 'a normal item' do |expected_quality_change|
+  shared_examples 'an item with quality change' do |expected_quality_change|
     it 'does not change the name' do
       expect(@items[0].name).to eq name
     end
@@ -22,117 +22,11 @@ describe GildedRose do
     end
 
     it "changes quality by #{expected_quality_change}" do
-      expect(@items[0].quality).to eq [quality + expected_quality_change, 50].min
+      expect(@items[0].quality).to eq quality + expected_quality_change
     end
   end
 
-  shared_examples 'a backstage pass' do |expected_quality|
-    it 'does not change the name' do
-      expect(@items[0].name).to eq name
-    end
-
-    it 'decreases sell_in by 1' do
-      expect(@items[0].sell_in).to eq sell_in - 1
-    end
-
-    it "sets quality to #{expected_quality}" do
-      expect(@items[0].quality).to eq expected_quality
-    end
-  end
-
-  context 'Normal Items' do
-    let(:name) { 'foo' }
-
-    context 'not overdue' do
-      let(:sell_in) { 5 }
-      let(:quality) { 10 }
-      include_examples 'a normal item', -1
-    end
-
-    context 'overdue' do
-      let(:sell_in) { 0 }
-      let(:quality) { 10 }
-      include_examples 'a normal item', -2
-    end
-
-    context 'with zero quality' do
-      let(:sell_in) { 5 }
-      let(:quality) { 0 }
-      include_examples 'a normal item', 0
-    end
-  end
-
-  context 'Conjured Items' do
-    let(:name) { 'Conjured Mana Cake' }
-
-    context 'not overdue' do
-      let(:sell_in) { 5 }
-      let(:quality) { 10 }
-      include_examples 'a normal item', -2
-    end
-
-    context 'overdue' do
-      let(:sell_in) { 0 }
-      let(:quality) { 10 }
-      include_examples 'a normal item', -4
-    end
-  end
-
-  context 'Aged Brie' do
-    let(:name) { 'Aged Brie' }
-
-    context 'before sell_in period' do
-      let(:sell_in) { 5 }
-      let(:quality) { 10 }
-      include_examples 'a normal item', 1
-    end
-
-    context 'after sell_in period' do
-      let(:sell_in) { 0 }
-      let(:quality) { 10 }
-      include_examples 'a normal item', 2
-    end
-
-    context 'with high quality' do
-      let(:sell_in) { 5 }
-      let(:quality) { 50 }
-      include_examples 'a normal item', 0
-    end
-  end
-
-  context 'Backstage passes to a TAFKAL80ETC concert' do
-    let(:name) { 'Backstage passes to a TAFKAL80ETC concert' }
-
-    context 'more than 10 days before concert' do
-      let(:sell_in) { 15 }
-      let(:quality) { 10 }
-      include_examples 'a normal item', 1
-    end
-
-    context '6 to 10 days before concert' do
-      let(:sell_in) { 10 }
-      let(:quality) { 10 }
-      include_examples 'a normal item', 2
-    end
-
-    context '1 to 5 days before concert' do
-      let(:sell_in) { 5 }
-      let(:quality) { 10 }
-      include_examples 'a normal item', 3
-    end
-
-    context 'after concert' do
-      let(:sell_in) { 0 }
-      let(:quality) { 10 }
-      include_examples 'a backstage pass', 0
-    end
-  end
-
-  context 'Sulfuras, Hand of Ragnaros' do
-    let(:name) { 'Sulfuras, Hand of Ragnaros' }
-    let(:sell_in) { 5 }
-    let(:quality) { 80 }
-
+  shared_examples 'an item with fixed quality' do
     it 'does not change the name' do
       expect(@items[0].name).to eq name
     end
@@ -144,5 +38,101 @@ describe GildedRose do
     it 'does not change the quality' do
       expect(@items[0].quality).to eq quality
     end
+  end
+
+  context 'Normal Items' do
+    let(:name) { 'foo' }
+
+    context 'not overdue' do
+      let(:sell_in) { 5 }
+      let(:quality) { 10 }
+      include_examples 'an item with quality change', -1
+    end
+
+    context 'overdue' do
+      let(:sell_in) { 0 }
+      let(:quality) { 10 }
+      include_examples 'an item with quality change', -2
+    end
+
+    context 'with zero quality' do
+      let(:sell_in) { 5 }
+      let(:quality) { 0 }
+      include_examples 'an item with quality change', 0
+    end
+  end
+
+  context 'Conjured Items' do
+    let(:name) { 'Conjured Mana Cake' }
+
+    context 'not overdue' do
+      let(:sell_in) { 5 }
+      let(:quality) { 10 }
+      include_examples 'an item with quality change', -2
+    end
+
+    context 'overdue' do
+      let(:sell_in) { 0 }
+      let(:quality) { 10 }
+      include_examples 'an item with quality change', -4
+    end
+  end
+
+  context 'Aged Brie' do
+    let(:name) { 'Aged Brie' }
+
+    context 'before sell_in period' do
+      let(:sell_in) { 5 }
+      let(:quality) { 10 }
+      include_examples 'an item with quality change', 1
+    end
+
+    context 'after sell_in period' do
+      let(:sell_in) { 0 }
+      let(:quality) { 10 }
+      include_examples 'an item with quality change', 2
+    end
+
+    context 'with high quality' do
+      let(:sell_in) { 5 }
+      let(:quality) { 50 }
+      include_examples 'an item with quality change', 0
+    end
+  end
+
+  context 'Backstage passes to a TAFKAL80ETC concert' do
+    let(:name) { 'Backstage passes to a TAFKAL80ETC concert' }
+
+    context 'more than 10 days before concert' do
+      let(:sell_in) { 15 }
+      let(:quality) { 10 }
+      include_examples 'an item with quality change', 1
+    end
+
+    context '6 to 10 days before concert' do
+      let(:sell_in) { 10 }
+      let(:quality) { 10 }
+      include_examples 'an item with quality change', 2
+    end
+
+    context '1 to 5 days before concert' do
+      let(:sell_in) { 5 }
+      let(:quality) { 10 }
+      include_examples 'an item with quality change', 3
+    end
+
+    context 'after concert' do
+      let(:sell_in) { 0 }
+      let(:quality) { 10 }
+      include_examples 'an item with quality change', -10
+    end
+  end
+
+  context 'Sulfuras, Hand of Ragnaros' do
+    let(:name) { 'Sulfuras, Hand of Ragnaros' }
+    let(:sell_in) { 5 }
+    let(:quality) { 80 }
+
+    include_examples 'an item with fixed quality'
   end
 end
